81d375fdf48ae1ef804fd5cdffc9106c793c7880
mohamed yasser <mohamed135095@eng.psu.edu.eg>
2024-09-04 20:37:17 +0300

    adding login

--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,28 +1,29 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { response } from 'express';
 import { catchError, throwError } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
-emailerrormessage: string ='';
-passworderrormessage: string ='';
-passworderrormessagearray: string[] = [];
+
 
   constructor(private http:HttpClient) { }
   registerUser(user: any) {
     return this.http.post('http://localhost:5062/Api/Account/register', user).pipe(
       catchError((error) => {
-        // Handle error here and return as observable
 
-        
-        
-        if (error.error.detail && error.error.detail.includes('Email')) {
-          this.emailerrormessage = error.error.detail;
-        }
-        if (error.error.ConfirmPassword) {
-          this.passworderrormessagearray = error.error.ConfirmPassword;
-        }
+        // Return error observable
+        return throwError(() => error);
+      })
+    );
+  }
+
+  loginUser(user: any) {
+    return this.http.post('http://localhost:5062/Api/Account/login', user).pipe(
+
+      catchError((error) => {
+
         // Return error observable
         return throwError(() => error);
       })
