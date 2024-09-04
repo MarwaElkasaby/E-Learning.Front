b923d200874365737c1fead0d617dc90a01a2a20
mohamed yasser <mohamed135095@eng.psu.edu.eg>
2024-09-04 19:50:39 +0300

    adding sign-up functionality

--- src/app/pages/register/register.component.ts
+++ src/app/pages/register/register.component.ts
@@ -1,12 +1,101 @@
-import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule, NgForm } from '@angular/forms';
+import { UserService } from '../../services/user.service';
 
 @Component({
   selector: 'app-register',
   standalone: true,
-  imports: [],
+  imports: [CommonModule,FormsModule],
   templateUrl: './register.component.html',
   styleUrl: './register.component.css'
 })
-export class RegisterComponent {
+export class RegisterComponent implements OnInit {
+  emailerrormessage: string ='';
+  passworderrormessage: string ='';
+  confirmPassworderrormessage: string ='';
+  passworderrormessagearray: string[] = [];
+
+registerform!:{
+  fName: string,
+  LName: string,
+  email: string,
+  password: string,
+  confirmPassword: string,
+  phone : number,
+  userRole: number
+}
+
+constructor(private userservice:UserService) {
+
+ }
+
+ngOnInit(): void {
+  this.registerform = {
+    fName: '',
+    LName: '',
+    email: '',
+    password: '',
+    confirmPassword: '',
+    phone: 0,
+    userRole: 0
+    
+  }
+}
+
+  // Helper method to clear all error messages
+  clearErrorMessages() {
+    this.emailerrormessage = '';
+    this.passworderrormessage = '';
+    this.confirmPassworderrormessage = '';
+    this.passworderrormessagearray = [];
+  }
+
+onSubmit(form: NgForm) {
+  this.userservice.registerUser(form.value).subscribe({
+    next: (response) => {
+      console.log('Registration successful', response);
+    },
+    error: (error) => {
+      console.log('Error in registration', error);
+
+
+        if (error.error && error.error.errors) {
+
+      //password confirm error done
+      if (error.error.errors.ConfirmPassword ) {
+  this.clearErrorMessages();
+        this.confirmPassworderrormessage = error.error.errors.ConfirmPassword;
+      console.log(error.error.errors.ConfirmPassword[0]);
+      
+      }
+        }
+
+
+      if (error.error.detail) {
+        console.log(error.error.detail);
+        
+
+        if (error.error.detail.includes('Username')) {
+          this.clearErrorMessages();
+          this.emailerrormessage = error.error.detail;
+
+        }
+
+        if (error.error.detail.includes('Passwords')) {
+          this.clearErrorMessages();
+          this.passworderrormessage = error.error.detail;
+        }
+        
+      }
+    }
+  });
+
 
 }
+
+
+
+
+}
+
--- src/app/services/user.service.ts
+++ src/app/services/user.service.ts
@@ -1,9 +1,31 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-
+import { catchError, throwError } from 'rxjs';
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
+emailerrormessage: string ='';
+passworderrormessage: string ='';
+passworderrormessagearray: string[] = [];
 
-  constructor() { }
+  constructor(private http:HttpClient) { }
+  registerUser(user: any) {
+    return this.http.post('http://localhost:5062/Api/Account/register', user).pipe(
+      catchError((error) => {
+        // Handle error here and return as observable
+
+        
+        
+        if (error.error.detail && error.error.detail.includes('Email')) {
+          this.emailerrormessage = error.error.detail;
+        }
+        if (error.error.ConfirmPassword) {
+          this.passworderrormessagearray = error.error.ConfirmPassword;
+        }
+        // Return error observable
+        return throwError(() => error);
+      })
+    );
+  }
 }
