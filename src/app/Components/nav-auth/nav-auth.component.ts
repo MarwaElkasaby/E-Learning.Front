bc2baad40db88a97452b0e06d681b6d19d82f634
mohamed yasser <mohamed135095@eng.psu.edu.eg>
2024-09-13 23:18:54 +0300

    adding logout

--- src/app/Components/nav-auth/nav-auth.component.ts
+++ src/app/Components/nav-auth/nav-auth.component.ts
@@ -1,6 +1,7 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { UserService } from '../../shared/services/user.service';
 @Component({
   selector: 'app-nav-auth',
   standalone: true,
@@ -9,6 +10,33 @@ import { RouterLink } from '@angular/router';
   styleUrl: './nav-auth.component.css'
 })
 export class NavAuthComponent  {
+  token: any;
+  isauth:boolean=false;
+  constructor(private userservice :UserService) {
+    if (typeof window !== 'undefined') {
+       this.token = localStorage.getItem('token');
+      if (this.token) {
+        this.isauth=true;
+      }
+    }else{
+      this.isauth=false;
+    }
+   }
+
+signout() {
+if (typeof window != 'undefined') {
+  localStorage.removeItem('token');
+  this.userservice.logout().subscribe({
+    next: (response)=>{
+      console.log(response);
+      this.isauth=false;
+    } , 
+    error: (err)=>{
+      console.log(err);
+    }
+  })
+}
+}
 
 
 
--- src/app/Components/nav-blank/nav-blank.component.ts
+++ src/app/Components/nav-blank/nav-blank.component.ts
@@ -5,6 +5,7 @@ import { RouterLink } from '@angular/router';
 import { CategoryService } from '../../shared/services/category.service';
 import { NgFor } from '@angular/common';
 import { Router } from '@angular/router';
+import { UserService } from '../../shared/services/user.service';
 @Component({
   selector: 'app-nav-blank',
   standalone: true,
@@ -13,26 +14,36 @@ import { Router } from '@angular/router';
   styleUrl: './nav-blank.component.css'
 })
 export class NavBlankComponent implements OnInit {
+  signout() {
+    if (typeof window != 'undefined') {
+      localStorage.removeItem('token');
+      this.userservice.logout().subscribe({
+        next: (response)=>{
+          console.log(response);
+          this.isauth=false;
+        } , 
+        error: (err)=>{
+          console.log(err);
+        }
+      })
+    }
+    }
+  
+  isauth:boolean=false;
   token: any;
   tokendata: any;
   userId !: number;
   username !: string;
   role !: string;
-  constructor (private _CategoryService:CategoryService, private _Router:Router){
-if(typeof window !== 'undefined') {
-  this.token = localStorage.getItem('token');
-}
-    
-    if (this.token) {
-      this.tokendata = JSON.parse(atob(this.token.split('.')[1]));
-      
-      // Extracting user ID, username, and role
-      this.userId = this.tokendata['http://schemas.xmlsoap.org/ws/2005/05/identity/claims/nameidentifier'];
-      this.username = this.tokendata['sub']; // Username claim
-      this.role = this.tokendata['http://schemas.microsoft.com/ws/2008/06/identity/claims/role']; // Role claim
-      console.log(this.userId);
-      
-    }
+  constructor (private _CategoryService:CategoryService, private _Router:Router,private userservice :UserService){
+    if (typeof window !== 'undefined') {
+      this.token = localStorage.getItem('token');
+     if (this.token) {
+       this.isauth=true;
+     }
+   }else{
+     this.isauth=false;
+   }
   }
 
 categories:any[]=[];
