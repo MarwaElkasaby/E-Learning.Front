087e9c83ef36862d7ea6078783182fb349117060
Mohamed El-Serafy <elserafy02@gmail.com>
2024-09-15 20:19:12 +0300

    share button

--- src/app/Components/nav-course/nav-course.component.ts
+++ src/app/Components/nav-course/nav-course.component.ts
@@ -1,6 +1,8 @@
 import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { RouterLink } from '@angular/router';
+import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
+import { ShareModalComponent } from '../share-modal/share-modal.component';
 
 @Component({
   selector: 'app-nav-course',
@@ -15,14 +17,18 @@ export class NavCourseComponent {
   @Input() progressPercentage?: number;
   isNavbarOpen = false;
 
+  constructor(private modalService: NgbModal) {}
+
   toggleNavbar() {
     this.isNavbarOpen = !this.isNavbarOpen;
   }
 
-  copyShareLink() {
-    const courseLink = `http://localhost:4200/course/${this.courseId}`;
-    navigator.clipboard.writeText(courseLink).then(() => {
-      alert('Course link copied to clipboard!');
+
+  openShareModal() {
+    const shareLink = `http://localhost:4200/course/${this.courseId}`;
+    const modalRef = this.modalService.open(ShareModalComponent, {
+      size: 'md',
     });
+    modalRef.componentInstance.shareLink = shareLink;
   }
 }
