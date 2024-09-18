095a29a6aab42644cd0488e73b98806cae42e480
Mohamed El-Serafy <elserafy02@gmail.com>
2024-09-18 04:01:40 +0300

    remove unused code

--- src/app/Components/nav-admin/nav-admin.component.ts
+++ src/app/Components/nav-admin/nav-admin.component.ts
@@ -1,35 +1,18 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
-import { Route, Router, RouterLink } from '@angular/router';
-import { UserService } from '../../shared/services/user.service';
-import { CookieService } from 'ngx-cookie-service';
-import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-nav-admin',
   standalone: true,
-  imports: [CommonModule, RouterLink, NgbCollapseModule, RouterLink],
+  imports: [CommonModule, RouterLink],
   templateUrl: './nav-admin.component.html',
   styleUrl: './nav-admin.component.css',
 })
 export class NavAdminComponent {
   constructor(
-    private userservice: UserService,
-    private cookieservice: CookieService,
-    private router: Router
   ) {}
 
-  isMenuCollapsed = true;
 
-  logout() {
-    this.userservice.logout().subscribe({
-      next: (response) => {
-        if (typeof window !== 'undefined') {
-          localStorage.removeItem('token');
-          this.cookieservice.delete('taalam');
-          this.router.navigate(['/login']);
-        }
-      },
-    });
-  }
+
 }
