fe4763542e95008ba308a6c25a156de77ce77f36
Mohamed El-Serafy <elserafy02@gmail.com>
2024-09-18 03:56:28 +0300

    remove admin navbar

--- src/app/Components/nav-admin/nav-admin.component.ts
+++ src/app/Components/nav-admin/nav-admin.component.ts
@@ -3,31 +3,33 @@ import { Component } from '@angular/core';
 import { Route, Router, RouterLink } from '@angular/router';
 import { UserService } from '../../shared/services/user.service';
 import { CookieService } from 'ngx-cookie-service';
+import { NgbCollapseModule } from '@ng-bootstrap/ng-bootstrap';
 
 @Component({
   selector: 'app-nav-admin',
   standalone: true,
-  imports: [CommonModule, RouterLink],
+  imports: [CommonModule, RouterLink, NgbCollapseModule, RouterLink],
   templateUrl: './nav-admin.component.html',
   styleUrl: './nav-admin.component.css',
 })
 export class NavAdminComponent {
-constructor(private userservice :UserService , private cookieservice:CookieService , private router :Router) {}
+  constructor(
+    private userservice: UserService,
+    private cookieservice: CookieService,
+    private router: Router
+  ) {}
+
+  isMenuCollapsed = true;
 
   logout() {
     this.userservice.logout().subscribe({
-   
-       next:(response)=>{
-   if (typeof window !== 'undefined') {
-         localStorage.removeItem('token');
-         this.cookieservice.delete('taalam');
-         this.router.navigate(['/login']);
-       }
-   
-     
-   }
-     })
-       }
-
-
+      next: (response) => {
+        if (typeof window !== 'undefined') {
+          localStorage.removeItem('token');
+          this.cookieservice.delete('taalam');
+          this.router.navigate(['/login']);
+        }
+      },
+    });
+  }
 }
